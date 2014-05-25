06e8dd7ec59e73a743186ee87439854042b66ca0
walter <walter@example.com>
2014-05-24 20:40:48 -0400

    style css and layouthtml

--- src/CallMe/WebBundle/Controller/DefaultController.php
+++ src/CallMe/WebBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('CallMeWebBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('CallMeWebBundle:Default:index.html.twig');
     }
 }
