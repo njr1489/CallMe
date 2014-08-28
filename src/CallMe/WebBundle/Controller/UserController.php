5f9a0e4f06f94cba8a5616c582b73790e19a1823
adamc <adamc@adamc-VirtualBox.(none)>
2014-08-27 22:30:37 -0400

    finished update action

--- src/CallMe/WebBundle/Controller/UserController.php
+++ src/CallMe/WebBundle/Controller/UserController.php
@@ -18,11 +18,22 @@ class UserController extends Controller
     public function settingsAction()
     {
         $user = $this->getUser();
-        return $this->render('CallMeWebBundle:User:settings.html.twig', ['first_name' => $user->getFirstName()]);
+        return $this->render('CallMeWebBundle:User:settings.html.twig', [
+            'id' => $user->getId(),
+            'first_name' => $user->getFirstName(),
+            'last_name' => $user->getLastName(),
+            'email' => $user->getEmail()
+        ]);
     }
 
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
     public function processSettingsAction(Request $request)
     {
-
+        $data = $request->request->all();
+        $this->get('user_manager')->updateUser($data);
+        return $this->redirect($this->generateUrl('user_settings'));
     }
 }
